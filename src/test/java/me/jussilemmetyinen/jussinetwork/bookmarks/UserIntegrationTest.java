f920a65d198df62f0578c25c290e97fd3e7f9b25
Jussi Lemmetyinen <jussi.lem@gmail.com>
2018-09-10 22:56:29 +0300

    Test writing, still failing partly

---
+++ src/test/java/me/jussilemmetyinen/jussinetwork/bookmarks/UserIntegrationTest.java
@@ -0,0 +1,4 @@
+package me.jussilemmetyinen.jussinetwork.bookmarks;
+
+public class UserIntegrationTest {
+}
---
+++ src/test/java/me/jussilemmetyinen/jussinetwork/bookmarks/spring/TestIntegrationConfig.java
@@ -0,0 +1,4 @@
+package me.jussilemmetyinen.jussinetwork.bookmarks.spring;
+
+public class TestIntegrationConfig {
+}
