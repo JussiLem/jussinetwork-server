c5d000f70963d1b3985aaabcb473055c47067b7c
Jussi Lemmetyinen <jussi.lem@gmail.com>
2018-09-10 23:35:31 +0300

    Privilege and Role entity classes

---
+++ src/main/java/me/jussilemmetyinen/jussinetwork/bookmarks/domain/Privilege.java
@@ -0,0 +1,69 @@
+package me.jussilemmetyinen.jussinetwork.bookmarks.domain;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+public class Privilege {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "privileges")
+    private Collection<Role> roles;
+
+    public Privilege() { super();}
+
+    public Privilege(final String name) {
+        super();
+        this.name = name;
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(final Long id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(final String name) { this.name = name; }
+
+    public Collection<Role> getRoles() { return roles; }
+
+    public void setRoles(final Collection<Role> roles) { this.roles = roles; }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        } if (object == null) {
+            return false;
+        } if (getClass() != object.getClass()) {
+            return false;
+        }
+        Privilege other = (Privilege) object;
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            } else if (!name.equals(other.name)) {
+                return false;
+            }
+        } return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Privilege [name=" + name +
+                "]" + "[id=" + id + "]";
+    }
+}
